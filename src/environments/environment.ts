08ed291f1e6faaddb013d71b6d47009279b1451e
KonKyaw <kaungkyaw1994@gmail.com>
2023-09-22 09:47:00 +0900

    Change Firebase Real-time Database settings

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    production: false,
-  firebase: {
-    projectId: 'wardrobe-inventory-42f37',
-    appId: '1:115492683025:web:d8a870251576338824a0b0',
-    databaseURL: 'https://wardrobe-inventory-42f37-default-rtdb.asia-southeast1.firebasedatabase.app',
-    storageBucket: 'wardrobe-inventory-42f37.appspot.com',
-    apiKey: 'AIzaSyCIOQuCA7R0V6EbvKsNAOytqdPeTjOprtA',
-    authDomain: 'wardrobe-inventory-42f37.firebaseapp.com',
-    messagingSenderId: '115492683025',
-    measurementId: 'G-J0N9DJFCQE',
-  }
+    production: true,
+    firebase: {
+      apiKey: "AIzaSyCfC3ScFCaDL8274ZTXuxeC7jgJjYjVO8g",
+      authDomain: "wardrobe-inventory-15a18.firebaseapp.com",
+      databaseURL: "https://wardrobe-inventory-15a18-default-rtdb.firebaseio.com",
+      projectId: "wardrobe-inventory-15a18",
+      storageBucket: "wardrobe-inventory-15a18.appspot.com",
+      messagingSenderId: "689431962919",
+      appId: "1:689431962919:web:fea34a4af078f59083f502",
+      measurementId: "G-2YS73Y52BD"
+    }
 };
